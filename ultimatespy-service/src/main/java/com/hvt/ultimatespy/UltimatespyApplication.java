304ea766607799359d28dca49660c14bb3618636
Ha Van Thai <thaihv@onepay.vn>
2020-08-05 18:18:46 +0700

    folder restructure

---
+++ ultimatespy-service/src/main/java/com/hvt/ultimatespy/UltimatespyApplication.java
@@ -0,0 +1,13 @@
+package com.hvt.ultimatespy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimatespyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimatespyApplication.class, args);
+	}
+
+}
---
+++ ultimatespy-service/src/test/java/com/hvt/ultimatespy/UltimatespyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvt.ultimatespy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimatespyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
